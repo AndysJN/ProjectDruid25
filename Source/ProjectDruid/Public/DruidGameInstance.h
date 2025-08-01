839ea4a37e88cacf8d20f71917bc47f222d67238
AndysJN <andrewsjn@gmail.com>
2025-08-02 00:42:46 +0200

    Druid Game Instance and Game Mode classes created

---
+++ Source/ProjectDruid/Public/DruidGameInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "DruidGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDRUID_API UDruidGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
