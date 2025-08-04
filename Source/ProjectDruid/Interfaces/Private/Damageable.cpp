eb8ae871dc00ce778798fcb112b7499f1d6f8d83
Kaan Ayyıldız(xTriplex) <kaan_xtriplex@hotmail.com>
2025-08-04 10:43:35 +0300

    Added an interface to help actors communicate with each other.
    Implemented the basic scaffolding for enemies dealing damage and the player taking damage.

---
+++ Source/ProjectDruid/Interfaces/Private/Damageable.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/Public/Damageable.h"
+
+
+// Add default functionality here for any IIDamageable functions that are not pure virtual.
---
+++ Source/ProjectDruid/Interfaces/Public/Damageable.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Damageable.generated.h"
+
+UINTERFACE()
+class UDamageable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PROJECTDRUID_API IDamageable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void TakeDamage(AActor* DamageCauser, float DamageAmount) = 0;
+};
